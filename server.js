41c83e102d620a59ec3aba9fb1e9bc7ca66eb47a
jlb1999 <jlbroughton88@gmail.com>
2019-10-13 07:10:17 -0400

    test

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-  app.get("/", (req, res) => {
-    res.send("Hello from server")
-})
-
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
     app.get("/*", (req, res) => {
