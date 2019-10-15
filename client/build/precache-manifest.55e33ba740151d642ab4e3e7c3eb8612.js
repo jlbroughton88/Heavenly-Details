ff9d82892f69e463bab623103293d007543ab083
jlb1999 <jlbroughton88@gmail.com>
2019-10-15 09:38:29 -0400

    new build

--- client/build/precache-manifest.55e33ba740151d642ab4e3e7c3eb8612.js
+++ client/build/precache-manifest.55e33ba740151d642ab4e3e7c3eb8612.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17bcf0670a6ef39b12a4e7d866065724",
+    "revision": "e2d70b9be6c0448aad77807c0a749824",
     "url": "/index.html"
   },
   {
-    "revision": "12b2ffe1120a0d976413",
-    "url": "/static/css/main.ea21e9c7.chunk.css"
+    "revision": "44ee086153a1682abf7b",
+    "url": "/static/css/main.d79a9db4.chunk.css"
   },
   {
-    "revision": "89916ca84265f1232ad6",
-    "url": "/static/js/2.6e9cc132.chunk.js"
+    "revision": "0a3fc5a0a84d70380ca7",
+    "url": "/static/js/2.466882e7.chunk.js"
   },
   {
-    "revision": "12b2ffe1120a0d976413",
-    "url": "/static/js/main.0ab93463.chunk.js"
+    "revision": "44ee086153a1682abf7b",
+    "url": "/static/js/main.45459c66.chunk.js"
   },
   {
     "revision": "cbee8d858c084296ccff",
