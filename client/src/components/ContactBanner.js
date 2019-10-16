c6627998d47d5073676d24ac1ffcf05b16b72435
jlb1999 <jlbroughton88@gmail.com>
2019-10-16 16:37:53 -0400

    added contact form, added server npms and route

---
+++ client/src/components/ContactBanner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ContactBanner = () => {
+    return (
+        <MotherDiv>
+            <Heading>Contact Us!</Heading>
+            <Para>We're here to help and answer any question you might have. We look forward to hearing from you. 😄</Para>
+        </MotherDiv>
+    )
+}
+
+export default ContactBanner;
+
+const MotherDiv = styled.div`
+    height: 400px;
+    max-width: 625px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    background: #ffffff;
+
+    @media(max-width: 830px) {
+        width: 75%;
+    }
+`
+
+const Heading = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    margin-bottom: 20px;
+
+`
+
+const Para = styled.h3`
+    margin: 0;
+    font-size: 1.2rem;
+`
--- server.js
+++ server.js
@@ -1,6 +1,72 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+// Nodemailer stuff
+const nodemailer = require("nodemailer");
+const creds = require("./.env");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+transporter.verify((err, success) => {
+  if(err) {
+    console.log(err)
+  } else {
+    console.log("Server is ready to take messages")
+  }
+})
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/send", (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let message = req.body.message;
+  let content = `name: ${name} \n email: ${email} \n message: ${message}`
+
+  let mail = {
+    from : name,
+    to: "jlbroughton88@gmail.com",
+    subject: "Test from contact form", 
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if(err) {
+      res.json({
+        msg: "Message failed"
+      }) 
+    } else {
+      res.json({
+        msg: "Message succeeded!"
+      })
+    }
+  })
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
+
+
+
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
