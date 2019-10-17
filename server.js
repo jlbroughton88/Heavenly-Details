3c4b2eaaf3e441aa472dfb056c4f0c69dcb66d23
jlb1999 <jlbroughton88@gmail.com>
2019-10-16 21:34:56 -0400

    test

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require("path");
 const nodemailer = require("nodemailer");
 const creds = require("./.env");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.ethereal.email",
@@ -21,7 +20,7 @@ const transporter = nodemailer.createTransport({
 });
 
 transporter.verify((err, success) => {
-  if (err) {
+  if(err) {
     console.log(err)
   } else {
     console.log("Server is ready to take messages")
@@ -31,61 +30,22 @@ transporter.verify((err, success) => {
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"))
-  })
-} else {
-  app.use(express.static(path.join(__dirname, "/client/public")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"))
-  })
-}
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
-
-// app.post("/send", (req, res) => {
-//   let name = req.body.name;
-//   let email = req.body.email;
-//   let message = req.body.message;
-//   let content = `name: ${name} \n email: ${email} \n message: ${message}`
-
-//   let mail = {
-//     from: name,
-//     to: "jlbroughton88@gmail.com",
-//     subject: "Test from contact form",
-//     text: content
-//   }
-
-//   transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         msg: "Message failed"
-//       })
-//     } else {
-//       res.json({
-//         msg: "Message succeeded!"
-//       })
-//     }
-//   })
-// });
 
 
 
 
-
-
-
-
-
-
-
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"))
+    })
+  } else {
+    app.use(express.static(path.join(__dirname, "/client/public")));
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/public/index.html"))
+    })
+  }
 
 app.listen(process.env.PORT || 5001, () => {
-  console.log("Server listening on port 5001")
+    console.log("Server listening on port 5001")
 }) 
