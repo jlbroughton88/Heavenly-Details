c308dbc09a8d7a138144e1b3e31b8059fc0290cc
jlb1999 <jlbroughton88@gmail.com>
2019-10-19 21:22:26 -0400

    Fixed a bug that made .map result overlap the footers on each package page

--- client/src/components/pages/packagePages/Ceramic.js
+++ client/src/components/pages/packagePages/Ceramic.js
@@ -29,17 +29,16 @@ export default Ceramic;
 const MotherDiv = styled.div``
 
 const Container = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-text-align: center;
-flex-direction: column;
-margin-bottom: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  margin-bottom: 100px;
 `
 
 const IncludesDiv = styled.div`
-width: 100%;
-height: 100%;
+  width: 100%;
   display: grid;
   grid-template-columns: 45% 45%;
   grid-gap: 30px;
